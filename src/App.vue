17fb7504bb3bc9ef57bcb5d3c3edee8bda3a2757
cangyu1993 <1765244720@qq.com>
2018-09-12 22:25:39 +0800

    找不到login

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <div id="app">
-    <!-- <ul>
-      <li>
-        <router-link to="/">首页</router-link>
-      </li>
-      <li>
-          <router-link to="/xuyong/223">详情页</router-link>
-      </li>
-    </ul>
-    <router-view></router-view> -->
     <router-link to="/login">登陆</router-link>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
